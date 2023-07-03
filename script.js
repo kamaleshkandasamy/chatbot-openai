3cbbf825b68c47a0b6ee0df18fcdb34fa068228f
kamaleshkandasamy <kamaleshkandasamyk@gmail.com>
2023-07-03 13:59:20 +0530

    Testing with the actual OpenAI API

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ const chatLog = document.getElementById('chat-log');
 const userInput = document.getElementById('user-input');
 const sendBtn = document.getElementById('send-btn');
 
-// Function to append a message to the chat log
-function appendMessage(message, sender) {
+
+// Array to store user messages
+const userMessages = [];
+
+// Function to add a user message to the chat log
+function addUserMessage(message) {
     const messageElement = document.createElement('div');
-    messageElement.classList.add('message', sender);
-    messageElement.innerText = message;
+    messageElement.classList.add('message', 'user-message');
+    messageElement.innerHTML = `<span class="message-content">${message}</span>`;
     chatLog.appendChild(messageElement);
 }
+
+// Function to add a chatbot message to the chat log
+function addChatbotMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message', 'chatbot-message');
+    messageElement.innerHTML = `<span class="message-content">${message}</span>`;
+    chatLog.appendChild(messageElement);
+}
+
 
 // OpenAPI call to fetch the response
 function openaiCall(userMessage, user) {
@@ -22,14 +35,15 @@ function openaiCall(userMessage, user) {
         'Authorization': `Bearer ${apiKey}`
     };
 
-    const conversationContext = {
-        user: user,
-        message: userMessage
-    };
+    const conversationContext = userMessages.map((message) => ({
+        role: user,
+        content: message,
+      }));
+
     const postBody = {
         model: openaiModel,
         messages: conversationContext
-    }
+    };
 
     // Make a POST request to start the chat conversation
     fetch(apiUrl, {
@@ -41,35 +55,36 @@ function openaiCall(userMessage, user) {
         .then(data => {
             // Process the response data
             console.log(data);
-            const response = data.choice[0].message.content;
-            appendMessage(response, 'chatbot');
+            const response = data.choices[0].message.content;
+            addChatbotMessage(response);
         })
         .catch(error => {
             // Handle any errors
             console.error('Error:', error);
             const response = "Thanks for asking the question, something went wrong. Call human assitance for help @123423242";
-            appendMessage(response, 'chatbot');
+            addChatbotMessage(response);
         });
 }
 
-// Function to handle user input
-function handleUserInput() {
-    const message = userInput.value;
-    const user = 'user';
-    appendMessage(message, user);
-
-    openaiCall(message, user);
-
-    // Clear the user input field
-    userInput.value = '';
-}
-
-// Event listener for the send button click event
+// Event listener for the send button
 sendBtn.addEventListener('click', handleUserInput);
 
-// Event listener for the enter key press event
-userInput.addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-        handleUserInput();
-    }
+// Event listener for the "keydown" event on the userInput
+userInput.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    handleUserInput();
+  }
 });
+
+function handleUserInput() {
+  const userMessage = userInput.value.trim();
+  const user = 'user';
+
+  if (userMessage !== '') {
+    userMessages.push(userMessage); // Add user message to the array
+    addUserMessage(userMessage); // Display user message in the chat log
+    openaiCall(userMessage, user); // Make the OpenAI API call
+  }
+
+  userInput.value = ''; // Clear the user input field
+}
